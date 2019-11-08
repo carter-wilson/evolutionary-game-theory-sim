820a58d823ac9bbc00e07974bc589e55b60e9992
Carter Wilson <carter47w@gmail.com>
2019-11-08 12:47:09 -0500

    got graphs working

--- grapher.py
+++ grapher.py
@@ -1,3 +1,23 @@
 import matplotlib
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
+
+def mypause(interval):
+    backend = plt.rcParams['backend']
+    if backend in matplotlib.rcsetup.interactive_bk:
+        figManager = matplotlib._pylab_helpers.Gcf.get_active()
+        if figManager is not None:
+            canvas = figManager.canvas
+            if canvas.figure.stale:
+                canvas.draw()
+            canvas.start_event_loop(interval)
+            return
+
+def plot(f, data, xlab='Time', ylab='', overwrite=True):
+    plt.figure(f)
+    if overwrite:
+        plt.clf()
+        plt.xlabel(xlab)
+        plt.ylabel(ylab)
+    plt.plot(data)
+    mypause(0.001)
--- simluator.py
+++ simluator.py
@@ -1,16 +1,18 @@
 from environment import Environment
+import matplotlib.pyplot as plt
+from grapher import plot
 
-env = Environment(100, 80, .6, .1, 5, .1, .3)
+
+env = Environment(50, 45, .6, .1, 5, .1, .3)
 # Environment(ns,nh,hfv,hlfv,sh,bhd,hd)
+
+pop = []
+age = []
+
 
 for i in range(5000):
     env.step()
-    print(len(env.shrimp),end='\t')
-    # print(sum([s.dna['strength'] for s in env.shrimp])/len(env.shrimp))
-    # print(round(sum([s.dna['pbidc'] for s in env.shrimp])/len(env.shrimp),1),end='\t')
-    # print(round(sum([s.dna['ibidc'] for s in env.shrimp])/len(env.shrimp),1),end='\t')
-    print(round(sum([s.dna['pbidh'] for s in env.shrimp])/len(env.shrimp),1),end='\t')
-    print(round(sum([s.dna['ibidh'] for s in env.shrimp])/len(env.shrimp),1),end='\t')
-    # print(sum([s.health for s in env.shrimp])/len(env.shrimp))
-    # print('------')
-    print()
+    pop.append(len(env.shrimp))
+    # age.append(sum([]))
+    plot(0,pop)
+    plt.show(block=False)
